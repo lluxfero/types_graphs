0286ded3880240b39d90d05f63b7ddd343c71054
lluxfero <onastja02@gmail.com>
2022-05-11 23:12:57 +0300

    Добавьте файлы проекта.

---
+++ prroba/prroba.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <clocale>
+using namespace std;
+
+void show_desk(int** d, int n1, int n2) {
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++)
+            cout << d[i][j] << " ";
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n = 7;
+    int d[7][7] = { {0, 1, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0},
+                    {1, 0, 0, 0, 1, 0, 0},
+                    {0, 1, 1, 0, 0, 1, 0},
+                    {0, 0, 0, 1, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 1, 0} };
+    int s, maxs = 0;
+    for (int i = 0; i < n; i++) {
+        s = 0;
+        for (int j = 0; j < n; j++)
+            if (d[i][j] == 1) s++;
+        if (maxs < s) maxs = s;
+    }
+    
+    cout << maxs << endl;
+
+    int** av = new int* [n];
+    for (int i = 0; i < n; i++)
+        av[i] = new int[maxs];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < maxs; j++)
+            av[i][j] = 0;
+
+    for (int i = 0; i < n; i++) {
+        s = 0;
+        for (int j = 0; j < n; j++)
+            if (d[i][j] == 1) {
+                av[i][s] = j + 1;
+                s++;
+            }
+    }
+
+    cout << "Векторы смежности: " << endl;
+    show_desk(av, n, maxs);
+}
---
+++ types_graphs/types_graphs.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <clocale>
+using namespace std;
+
+int err_n() {
+    int m, ival = 1;
+    cin >> m;
+    while (ival == 1) {
+        if (cin.fail() || m < 1) {
+            cin.clear();
+            cin.ignore(32767, '\n');
+            cout << "Ошибка! Попробуйте еще раз: ";
+            cin >> m;
+        }
+        else
+            ival = 0;
+    }
+    return m;
+}
+
+int err_d() {
+    int m, ival = 1;
+    cin >> m;
+    while (ival == 1) {
+        if (cin.fail() || m < 0 || m > 1) {
+
+            cin.clear();
+            cin.ignore(32767, '\n');
+            cout << "Ошибка! Попробуйте еще раз: ";
+            cin >> m;
+        }
+        else
+            ival = 0;
+    }
+    return m;
+}
+
+void show_desk(int** d, int n1, int n2) {
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++)
+            cout << d[i][j] << " ";
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void adjacency_vectors(int** d, int n) {
+    int s, maxs = 0;
+    for (int i = 0; i < n; i++) {
+        s = 0;
+        for (int j = 0; j < n; j++)
+            if (d[i][j] == 1) s++;
+        if (maxs < s) maxs = s;
+    }
+
+    int** av = new int* [n];
+    for (int i = 0; i < n; i++)
+        av[i] = new int[maxs];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < maxs; j++)
+            av[i][j] = 0;
+
+    for (int i = 0; i < n; i++) {
+        s = 0;
+        for (int j = 0; j < n; j++)
+            if (d[i][j] == 1) {
+                av[i][s] = j + 1;
+                s++;
+            }
+    }
+
+    show_desk(av, n, maxs);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n;
+    cout << "Введите количество вершин дерева: ";
+    n = err_n();
+
+    int** desk = new int* [n];
+    for (int i = 0; i < n; i++)
+        desk[i] = new int[n];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            desk[i][j] = 0;
+
+    cout << "Введите матрицу смежности: " << endl;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            desk[i][j] = err_d();
+    cout << endl;
+
+    cout << "Векторы смежности: " << endl;;
+    adjacency_vectors(desk, n);
+
+}
